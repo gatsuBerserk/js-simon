e6861ad6dbf05863e33894ea0a1ea9f46aa466c9
gatsuBerserk <berserkdev@outlook.it>
2022-03-08 20:48:43 +0100

    control function (not fully satisfied)

--- js/script.js
+++ js/script.js
@@ -10,28 +10,49 @@
     // Schema mentale e svolgimento
 
     // 1) Inserire 5 numeri casuali in un array e stamparlo a schermo
-        const number= document.getElementById("dom-numbers"); 
+        const number= document.getElementById("dom-numbers");  
+        const play = document.getElementById("play");
+        const numberUtent=[];  
         const casualNumber=[]; 
-        for ( let i = 0; i < 5; i++){
-        casualNumber.push(Math.floor(Math.random()* 100));
-        };
-        console.log(casualNumber); 
+        // for ( let i = 0; i < 5; i++){
+        // casualNumber.push(Math.floor(Math.random()* 100));
+        // };
+        // console.log(casualNumber); 
         // Stampa su elemento Dom
-        number.innerHTML= casualNumber;
+        // number.innerHTML= casualNumber.join("-");
 
     // 2) inserire fuzione time(i numeri non si devono vedere)
-        // Funzione 
-        setTimeout(numeri, 2000) // prova di 2 secondi
-        function numeri (){
-         document.getElementById("dom-numbers").classList.add("d-none");
-        };
-    // 3) prompt utente (chiedo di inserire i 5 numeri)
-            const memoryUtent=[];  
+        // Funzione   
+        play.addEventListener("click", function(){ 
+            for ( let i = 0; i < 5; i++){
+                casualNumber.push(Math.floor(Math.random()* 100));
+                };
+                console.log(casualNumber); 
+                // Stampa su elemento Dom
+                number.innerHTML= casualNumber.join("-"); 
+        setTimeout(numeri, 5000) // prova di 2 secondi
+            function numeri (){
+            document.getElementById("dom-numbers").classList.add("d-none");
+            };
+    // 3) prompt utente (chiedo di inserire i 5 numeri)  
+            setTimeout(game, 6000 )
+            function game(){
+            const memoryUtent=[];   
             for (let i = 0; i < 5; i++){
-                const utente= parseInt(prompt("inserisci i numeri che ti ricordi")); 
-                memoryUtent.push(utente);
-            }
-            console.log(memoryUtent) 
-
-    // Comparazione degli array formati
-    
+                const utente= parseInt(prompt("inserisci i numeri che ti ricordi")) 
+                memoryUtent.push(utente) 
+                };
+                // Creo due variabili come contatore
+                let memories=0; 
+                let notRemember= 0; 
+                // Creo la funzione di comparazione 
+                for (let i = 0; i < memoryUtent.length; i++){
+                    if(casualNumber.includes( memoryUtent[i])){
+                        memories++
+                    }else{
+                        notRemember++
+                    } 
+                //Stampo nel Dom il risultato 
+                }document.getElementById("numeri").innerHTML=` Hai indovinato solo: ${memories}  di 5 numeri.  I numeri da ricordare erano: ${casualNumber.join(" ")}`;
+        }; 
+    });
